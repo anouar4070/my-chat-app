e1731b5d2bfe5fec99cb5683154c7a41d0ff8d83
anouar garbaa <g.anouar@yahoo.com>
2025-05-16 11:13:22 +0200

    adding NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "@/components/Providers";
+import TopNav from "@/components/TopNav";
 
 export const metadata: Metadata = {
   title: "Next Chat App",
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Providers>
+          <TopNav />
           {children}
         </Providers>
       </body>

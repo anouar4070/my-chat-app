c244c459a7acd4f3c7ccfc0635d81801fdff3280
anouar garbaa <g.anouar@yahoo.com>
2025-05-16 22:29:34 +0200

    Validation with Zod

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -6,15 +6,20 @@ import { Button } from "@heroui/button";
 import React from "react";
 import { GiPadlock } from "react-icons/gi";
 import { useForm } from "react-hook-form";
+import { loginSchema, LoginSchema } from "@/lib/schemas/loginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function LoginForm() {
   const {
     register,
     handleSubmit,
     formState: { errors, isValid },
-  } = useForm();
+  } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+    mode: "onTouched",
+  });
 
-  const onSubmit = (data: any) => {
+  const onSubmit = (data: LoginSchema) => {
     console.log(data);
   };
 
@@ -36,7 +41,7 @@ export default function LoginForm() {
               defaultValue=""
               label="Email"
               variant="bordered"
-              {...register("email", { required: "Email is required" })}
+              {...register("email")}
               isInvalid={!!errors.email}
               errorMessage={errors.email?.message as string}
             />
@@ -45,7 +50,7 @@ export default function LoginForm() {
               label="Password"
               variant="bordered"
               type="password"
-              {...register("password", { required: "Password is required" })}
+              {...register("password")}
               isInvalid={!!errors.password}
               errorMessage={errors.password?.message as string}
             />

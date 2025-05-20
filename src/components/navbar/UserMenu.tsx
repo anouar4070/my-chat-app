09981f64affa67b07b7885ae6f24698b112dc2c8
anouar garbaa <g.anouar@yahoo.com>
2025-05-20 22:53:29 +0200

    Adding a dropdown menu to the Nav bar Part1

---
+++ src/components/navbar/UserMenu.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import {
+  Avatar,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownSection,
+  DropdownTrigger,
+} from "@heroui/react";
+import Link from "next/link";
+import { Session } from "next-auth";
+import { signOut } from "next-auth/react";
+import React from "react";
+
+type Props = {
+  user: Session["user"];
+};
+
+export default function UserMenu({ user }: Props) {
+  return (
+    <Dropdown placement="bottom-end">
+      <DropdownTrigger>
+        <Avatar
+          isBordered
+          as="button"
+          className="transition-transform"
+          color="secondary"
+          name={user?.name || "user avatar"}
+          size="sm"
+          src={user?.image || "/images/user.png"}
+        />
+      </DropdownTrigger>
+      <DropdownMenu variant="flat" aria-label="User actions menu">
+        <DropdownSection showDivider>
+          <DropdownItem
+            key="signInAs"
+            isReadOnly
+            as="span"
+            className="h-14 flex flex-raw"
+            aria-label="username"
+          >
+            Signed in as {user?.name}
+          </DropdownItem>
+        </DropdownSection>
+        <DropdownItem key="editProfile" as={Link} href="/members/edit">
+          Edit profile
+        </DropdownItem>
+        <DropdownItem
+          key="logOut"
+          color="danger"
+          onPress={async () => signOut()}
+        >
+          Log out
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}

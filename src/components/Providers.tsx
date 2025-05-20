676eb17297774c6911fa83575195f79507fa7999
anouar garbaa <g.anouar@yahoo.com>
2025-05-20 19:43:31 +0200

    Adding notification toasts to the app

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,8 +1,15 @@
-'use client';
+"use client";
 
 import { HeroUIProvider } from "@heroui/react";
 import { ReactNode } from "react";
+import { ToastContainer } from "react-toastify";
+//import 'react-toastify/dist/ReactToastify.css';
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
+  return (
+    <HeroUIProvider>
+      <ToastContainer position='bottom-right' hideProgressBar className="z-50" />
+      {children}
+    </HeroUIProvider>
+  );
 }

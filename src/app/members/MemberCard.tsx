e01c26cf2d515c23072d0ed0e3f2fdc6ac19f57c
anouar garbaa <g.anouar@yahoo.com>
2025-05-24 15:05:22 +0200

    Creating a like button

--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -6,6 +6,7 @@ import { Member } from "@prisma/client";
 import React from "react";
 import Link from "next/link";
 import { calculateAge } from "@/lib/util";
+import LikeButton from "@/components/LikeButton";
 
 type Props = {
   member: Member;
@@ -21,6 +22,9 @@ export default function MemberCard({ member }: Props) {
         src={member.image || "/images/user.png"}
         className="aspect-square object-cover"
       />
+      <div className="absolute top-3 right-3 z-50 ">
+        <LikeButton targetId={member.userId} hasLiked={false} />
+      </div>
       <CardFooter className="flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient">
         <div className="flex flex-col text-white">
           <span className="font-semibold">

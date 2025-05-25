5a0ded9c6aec2a4f4fe4511d66c60332f74f2b51
anouar garbaa <g.anouar@yahoo.com>
2025-05-25 13:06:46 +0200

    Adding edit member form Prat 1

---
+++ src/app/members/edit/EditForm.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { MemberEditSchema, memberEditSchema } from "@/lib/schemas/memberEditSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Member } from "@prisma/client";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+type Props = {
+  member: Member;
+};
+
+export default function EditForm({ member }: Props) {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isValid, isDerty, isSubmitting },
+  } = useForm<MemberEditSchema>({ resolver: zodResolver(memberEditSchema) });
+  return <div>EditForm</div>;
+}
--- src/app/members/edit/page.tsx
+++ src/app/members/edit/page.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import { CardBody, CardHeader } from "@heroui/card";
+import { Divider } from "@heroui/divider";
+import EditForm from "./EditForm";
+import { getAuthUserId } from "@/app/actions/authActions";
+import { getMemberByUserId } from "@/app/actions/memberActions";
+import { notFound } from "next/navigation";
 
-export default function MemberEditPage() {
-  return <div>MemberEditPage</div>;
+export default async function MemberEditPage() {
+  const userId = await getAuthUserId();
+  const member = await getMemberByUserId(userId);
+
+  if (!member) return notFound();
+
+  return (
+    <>
+      <CardHeader className="text-2xl font-semibold text-secondary">
+        Edit Profile
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <EditForm member={member} />
+      </CardBody>
+    </>
+  );
 }

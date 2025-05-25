2233ff310c15f538f53907daa622350314cccbeb
anouar garbaa <g.anouar@yahoo.com>
2025-05-25 16:14:08 +0200

    Adding the buttons for the image actions

--- src/app/members/edit/photos/page.tsx
+++ src/app/members/edit/photos/page.tsx
@@ -4,6 +4,8 @@ import { Divider } from "@heroui/divider";
 import { getAuthUserId } from "@/app/actions/authActions";
 import { getMemberPhotosByUserId } from "@/app/actions/memberActions";
 import { Image } from "@heroui/image";
+import StarButton from "@/components/StarButton";
+import DeleteButton from "@/components/DeleteButton";
 
 export default async function PhotosPage() {
   const userId = await getAuthUserId();
@@ -21,6 +23,12 @@ export default async function PhotosPage() {
             photos.map((photo) => (
               <div key={photo.id} className="relative">
                 <Image width={220} src={photo.url} alt="Image of user" />
+                <div className="absolute top-3 left-3 z-50">
+                  <StarButton selected={false} loading={false} />
+                </div>
+                <div className="absolute top-3 right-3 z-50">
+                  <DeleteButton loading={false} />
+                </div>
               </div>
             ))}
         </div>

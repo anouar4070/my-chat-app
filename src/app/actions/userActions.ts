36530dec3a617a1af46123605e0dc7fc3c8fb956
anouar garbaa <g.anouar@yahoo.com>
2025-05-27 00:10:47 +0200

    Setting the main image

--- src/app/actions/userActions.ts
+++ src/app/actions/userActions.ts
@@ -6,7 +6,7 @@ import {
 } from "@/lib/schemas/memberEditSchema";
 import { ActionResult } from "@/types";
 import { getAuthUserId } from "./authActions";
-import { Member } from "@prisma/client";
+import { Member, Photo } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
 
 export async function updateMemberProfile(
@@ -56,6 +56,25 @@ export async function addImage(url: string, publicId: string) {
     })
   } catch (error) {
     console.log(error);
+    throw error;
+  }
+}
+
+export async function setMainImage(photo: Photo) {
+  try {
+    const userId = await getAuthUserId()
+
+    await prisma.user.update({
+      where: {id: userId},
+      data: {image: photo.url}
+    })
+
+    return prisma.member.update({
+      where: {userId},
+      data: {image: photo.url}
+    })
+  } catch (error) {
+      console.log(error);
     throw error;
   }
 }

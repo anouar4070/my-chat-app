d7765a368d5462b837ea6bcc6c423d49f5e2a157
anouar garbaa <g.anouar@yahoo.com>
2025-05-29 21:55:17 +0200

    Displaying the messages Part 2

--- src/app/members/[userId]/chat/MessageBox.tsx
+++ src/app/members/[userId]/chat/MessageBox.tsx
@@ -22,6 +22,36 @@ export default function MessageBox({ message, currentUserId }: Props) {
     />
   );
 
+  const messageContentClasses = clsx("flex flex-col w-[50%] px-2 py-1", {
+    "rounded-l-xl rounded-tr-xl text-white bg-blue-100": isCurrentUserSender,
+    "rounded-r-xl rounded-tl-xl border-gray-200 bg-green-100":
+      !isCurrentUserSender,
+  });
+
+ const renderMessageHeader = () =>  (
+      <div className={clsx('flex items-center w-full', {
+        'justify-between': isCurrentUserSender
+      })}>
+   {message.dateRead && message.recipientId !== currentUserId ? (
+    <span className="text-xs text-black text-italic">(Read 4 mins ago)</span>
+   ) : <div></div>}
+   <div className="flex">
+    <span className="text-sm font-semibold text-gray-900">{message.senderName}</span>
+    <span className="text-sm text-gray-500 ml-2">{message.created}</span>
+   </div>
+      </div>
+    );
+  
+
+  const renderMessageContent = () => {
+    return (
+      <div className={messageContentClasses}>
+        {renderMessageHeader()}
+        <p className="text-sm py-3 text-gray-900">{message.text}</p>
+      </div>
+    );
+  };
+
   return (
     <div className="grid grid-rows-1">
       <div
@@ -31,9 +61,31 @@ export default function MessageBox({ message, currentUserId }: Props) {
         })}
       >
         {!isCurrentUserSender && renderAvatar()}
-        <div>Message content</div>
+        {renderMessageContent()}
         {isCurrentUserSender && renderAvatar()}
       </div>
     </div>
   );
 }
+
+
+
+/**
+ * explanation
+
+✅ Base:  <div className="flex items-center w-full">
+→ This container uses flexbox, which lets its child elements be aligned horizontally.
+
+✅ If you are the sender (isCurrentUserSender === true):
+Then we add justify-between.
+That means:
+→ The first child (the (Read …) block or the empty <div>) goes all the way to the left
+→ The second child (the block with senderName + created) goes all the way to the right
+Result:
+The name and time of your own message are aligned to the right.
+
+✅ If the other person sent the message (isCurrentUserSender === false):
+Then we don’t add justify-between.
+By default, flexbox aligns the elements to the left
+→ So the name and time stay aligned on the left.
+ */

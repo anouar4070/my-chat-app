cece565fe7feeaca0e04737f3559e6d84166a321
anouar garbaa <g.anouar@yahoo.com>
2025-05-30 12:34:53 +0200

    Creating the message table

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import MessageSidebar from "./MessageSidebar";
 import { getMessagesByContainer } from "../actions/messageActions";
+import MessageTable from "./MessageTable";
 
 export default async function MessagesPage({
   searchParams,
@@ -9,13 +10,15 @@ export default async function MessagesPage({
 }) {
   const { container } = await searchParams;
   const messages = await getMessagesByContainer(container);
-  console.log({ messages });
+
   return (
     <div className="grid grid-cols-12 gap-5 h-[80vh] mt-10">
       <div className="col-span-2">
         <MessageSidebar />
       </div>
-      <div className="col-span-10">Message table goes here</div>
+      <div className="col-span-10">
+        <MessageTable messages= {messages} />
+        </div>
     </div>
   );
 }

1ba982488fa649c22328d542f6ab401025a05d04
anouar garbaa <g.anouar@yahoo.com>
2025-05-31 18:51:07 +0200

    Adding the live chat functionality

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -45,3 +45,8 @@ export function truncateString(text?: string | null, num = 50) {
   }
   return text.slice(0, num) + "...";
 }
+
+// Generates a unique chat ID for two users regardless of the provided IDs order 
+export function createChatId(a: string, b: string) {
+  return a > b ? `${b}-${a}` : `${a}-${b}`
+}

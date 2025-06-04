bcbbf4385e65bea65cad8caa5145f58eacb4f491
anouar garbaa <g.anouar@yahoo.com>
2025-06-04 17:51:52 +0200

    Adding the realtime functionality to the message table

--- src/app/messages/MessageTable.tsx
+++ src/app/messages/MessageTable.tsx
@@ -8,11 +8,11 @@ import MessageTableCell from "./MessageTableCell";
 import { useMessages } from "@/hooks/useMessages";
 
 type Props = {
-  messages: MessageDto[];
+  initialMessages: MessageDto[];
 };
 
-export default function MessageTable({ messages }: Props) {
-  const { isOutbox, columns, deleteMessage, selectRow, isDeleting } = useMessages(messages);
+export default function MessageTable({ initialMessages }: Props) {
+  const { isOutbox, columns, deleteMessage, selectRow, isDeleting, messages } = useMessages(initialMessages);
 
   return (
     <Card className="flex flex-col gap-3 h-[80vh] overflow-auto">
--- src/hooks/useMessages.tsx
+++ src/hooks/useMessages.tsx
@@ -2,14 +2,32 @@ import { deleteMessage } from "@/app/actions/messageActions";
 import { MessageDto } from "@/types";
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/navigation";
-import { Key, useState, useCallback } from "react";
+import { Key, useState, useCallback, useEffect } from "react";
+import useMessageStore from "./useMessageStore";
+import { useShallow } from "zustand/shallow";
 
-
-export const useMessages = (messages: MessageDto[]) => {
-   const searchParams = useSearchParams();
+export const useMessages = (initialMessages: MessageDto[]) => {
+  const { set, remove, messages } = useMessageStore(
+    useShallow((state) => ({
+      set: state.set,
+      remove: state.remove,
+      messages: state.messages,
+    }))
+  );
+  const searchParams = useSearchParams();
   const router = useRouter();
   const isOutbox = searchParams.get("container") === "outbox";
   const [isDeleting, setDeleting] = useState({ id: "", loading: false });
+
+  useEffect(() => {
+    set(initialMessages);
+
+    return () => {
+      set([]);
+    };
+  }, [initialMessages, set]);
+
+
   // Define table columns depending on inbox/outbox
   const columns = [
     {
@@ -45,6 +63,7 @@ export const useMessages = (messages: MessageDto[]) => {
     columns,
     deleteMessage: handleDeleteMessage,
     selectRow: handleRowSelect,
-    isDeleting
-  }
-}
+    isDeleting,
+    messages
+  };
+};

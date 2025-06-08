9c9655be2f7b28efdf647292dc68d3343d77601a
anouar garbaa <g.anouar@yahoo.com>
2025-06-08 17:46:47 +0200

    Adding a filter hook

--- src/components/navbar/Filters.tsx
+++ src/components/navbar/Filters.tsx
@@ -1,5 +1,74 @@
 "use client";
 
+import { useFilters } from "@/hooks/useFilters";
+import { Button, Select, SelectItem, Slider} from "@heroui/react";
+import { usePathname} from "next/navigation";
+import React from "react";
+
+
+export default function Filters() {
+  const pathname = usePathname();
+  const { orderByList, genderList, selectAge, selectGender, selectOrder, filters} = useFilters()
+
+
+  if (pathname !== "/members") return null;
+
+  return (
+    <div className="shadow-md py-2">
+      <div className="flex flex-row justify-around items-center">
+        <div className="text-secondary font-semibold text-xl">Results: 10</div>
+        <div className="flex gap-2 items-center">
+          <div>Gender:</div>
+          {genderList.map(({ icon: Icon, value }) => (
+            <Button 
+            key={value} 
+            size="sm" 
+            isIconOnly
+             color={filters.gender.includes(value) ? 'secondary' : 'default'}
+             onPress={() => selectGender(value)}
+             >
+              <Icon size={24} />
+            </Button>
+          ))}
+        </div>
+        <div className="flex flex-row items-center gap-2 w-1/4">
+          <Slider
+            aria-label="slider for age selection"
+            label="Age range"
+            color="secondary"
+            size="sm"
+            minValue={18}
+            maxValue={100}
+            defaultValue={filters.ageRange}
+            onChangeEnd={(value) => selectAge(value as number[])}
+          />
+        </div>
+        <div className="w-1/4">
+          <Select
+            size="sm"
+            fullWidth
+            label="Order by"
+            variant="bordered"
+            color="secondary"
+            aria-label="Order by selector"
+            selectedKeys={new Set([filters.orderBy])}
+            onSelectionChange={selectOrder}
+          >
+            {orderByList.map((item) => (
+              <SelectItem key={item.value}>{item.label}</SelectItem>
+            ))}
+          </Select>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+//* ***     Version 1 without useFilters Hook   ***
+/**
+ "use client";
+
 import { Button, Select, SelectItem, Slider, Selection } from "@heroui/react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React from "react";
@@ -102,3 +171,5 @@ router.replace(`${pathname}?${params}`);
     </div>
   );
 }
+
+ */

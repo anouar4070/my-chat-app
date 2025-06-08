f117368289c2de44fd32ef8d7917c6154e72c0f2
anouar garbaa <g.anouar@yahoo.com>
2025-06-08 15:28:09 +0200

    Updating the last active property

--- src/app/actions/memberActions.ts
+++ src/app/actions/memberActions.ts
@@ -4,6 +4,7 @@ import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma";
 import { UserFilters } from "@/types";
 import { addYears } from "date-fns";
+import { getAuthUserId } from "./authActions";
 
 export async function getMembers(searchParams: UserFilters) {
   const session = await auth();
@@ -53,4 +54,18 @@ export async function getMemberPhotosByUserId(userId: string) {
   if (!member) return null;
 
   return member.photos;
+}
+
+export async function updateLastActive() {
+const userId = await getAuthUserId();
+
+  try {
+    return prisma.member.update({
+       where: { userId },
+       data: {updated: new Date()}
+      });
+  } catch (error) {
+    console.log(error);
+    throw error; // important for the Error component
+  }
 }
--- src/hooks/usePresenceChannel.ts
+++ src/hooks/usePresenceChannel.ts
@@ -1,68 +1,89 @@
 import { useCallback, useEffect, useRef } from "react";
-import usePresenceStore from "./usePresenceStore"
+import usePresenceStore from "./usePresenceStore";
 import { Channel, Members } from "pusher-js";
 import { pusherClient } from "@/lib/pusher";
-import {useShallow} from 'zustand/shallow';
+import { useShallow } from "zustand/shallow";
+import { updateLastActive } from "@/app/actions/memberActions";
 
 // Custom React hook to manage a Pusher presence channel
 export const usePresenceChannel = () => {
+  // Extract set, add, and remove functions from the presence store
+  const { set, add, remove } = usePresenceStore(
+    useShallow(
+      // <-- should be used with zustand 5
+      (state) => ({
+        set: state.set,
+        add: state.add,
+        remove: state.remove,
+      })
+    )
+  );
 
-  // Extract set, add, and remove functions from the presence store 
-  const {set, add, remove} = usePresenceStore(
-    useShallow(  // <-- should be used with zustand 5
-    state => ({
-    set: state.set,
-    add: state.add,
-    remove: state.remove,
-  })));
+  // Ref to hold the Pusher channel instance
+  const channelRef = useRef<Channel | null>(null);
 
-// Ref to hold the Pusher channel instance
-const channelRef = useRef<Channel | null>(null)
-
- // Memoized function to set the full list of member IDs in the store
-const handleSetMembers = useCallback((memberIds: string[]) => {
-set(memberIds)
-}, [set])
+  // Memoized function to set the full list of member IDs in the store
+  const handleSetMembers = useCallback(
+    (memberIds: string[]) => {
+      set(memberIds);
+    },
+    [set]
+  );
 
   // Memoized function to add a new member ID to the store
-const handleAddMember = useCallback((memberId: string) => {
-add(memberId)
-}, [add])
+  const handleAddMember = useCallback(
+    (memberId: string) => {
+      add(memberId);
+    },
+    [add]
+  );
 
-// Memoized function to remove a member ID from the store
-const handleRemoveMember = useCallback((memberId: string) => {
-remove(memberId)
-}, [remove])
+  // Memoized function to remove a member ID from the store
+  const handleRemoveMember = useCallback(
+    (memberId: string) => {
+      remove(memberId);
+    },
+    [remove]
+  );
 
- // Effect runs on mount to subscribe to the Pusher presence channel
+  // Effect runs on mount to subscribe to the Pusher presence channel
   useEffect(() => {
-    if(!channelRef.current){
-       // Subscribe to the 'presence-nm' channel
-      channelRef.current = pusherClient.subscribe('presence-nm')
+    if (!channelRef.current) {
+      // Subscribe to the 'presence-nm' channel
+      channelRef.current = pusherClient.subscribe("presence-nm");
 
       // When subscription succeeds, set the initial list of members
-      channelRef.current.bind('pusher:subscription_succeeded', (members: Members) => {
-        handleSetMembers(Object.keys(members.members))
-      })
+      channelRef.current.bind(
+        "pusher:subscription_succeeded",
+        async (members: Members) => {
+          handleSetMembers(Object.keys(members.members));
+          await updateLastActive();
+        }
+      );
 
       // When a new member joins, add him to the store
-       channelRef.current.bind('pusher:member_added', (member: {id: string}) => {
-        handleAddMember(member.id)
-      })
+      channelRef.current.bind(
+        "pusher:member_added",
+        (member: { id: string }) => {
+          handleAddMember(member.id);
+        }
+      );
 
       // When a member leaves, remove him from the store
-       channelRef.current.bind('pusher:member_removed', (member: {id: string}) => {
-        handleRemoveMember(member.id)
-      })
+      channelRef.current.bind(
+        "pusher:member_removed",
+        (member: { id: string }) => {
+          handleRemoveMember(member.id);
+        }
+      );
     }
 
     // Cleanup when the component unmounts
     return () => {
-      if(channelRef.current && channelRef.current.subscribed) {
+      if (channelRef.current && channelRef.current.subscribed) {
         channelRef.current.unsubscribe();
         channelRef.current.unbind_all();
       }
-    }
-  }, [handleAddMember, handleRemoveMember, handleSetMembers])
-  
-}
+    };
+  }, [handleAddMember, handleRemoveMember, handleSetMembers]);
+};
